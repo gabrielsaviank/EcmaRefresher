c7cc59c515437eecfaaaa7ce7a6f7badedbbeff3
GabrielSavian <gabriel@amplimed.com.br>
2020-11-05 10:35:17 -0300

    arrays

---
+++ Arrays & Methods/array.js
@@ -0,0 +1,15 @@
+//Array 
+
+const stuff = ["Sports", 1, true, {gabriel}];
+
+const hobbies = ["Sports", "Cooking"];
+
+for (let hobby of hobbies) {
+    console.log(hobby);
+}
+
+/*Map creates an array that takes a funciton where you define how to edit that array 
+or the elements, the funciton will be executed on every element of the array */
+console.log(hobbies.map( hobby => {
+    return 'Hobby: ' + hobby;
+}));
